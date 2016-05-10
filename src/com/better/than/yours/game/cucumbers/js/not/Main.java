6edf6d60f2629a820d68de713d6772b9e4baa95f
Kenish <przemo9712@gmail.com>
2016-05-10 15:35:25 +0200

    pierdoły

--- src/com/better/than/yours/game/cucumbers/js/not/Main.java
+++ src/com/better/than/yours/game/cucumbers/js/not/Main.java
@@ -1,8 +1,13 @@
 package com.better.than.yours.game.cucumbers.js.not;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+        Board board = new Board(10,10);
+        Engine engine = new Engine(board,10);
+        HashMap map = engine.getMap();
 
 	// write your code here
     }
