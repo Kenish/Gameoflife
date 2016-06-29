07b6f60f7700a4266b5fb411e8b3c71ee897eadb
Kenish <przemo9712@gmail.com>
2016-06-29 15:23:24 +0200

    Update Board.java

--- src/com/better/than/yours/game/cucumbers/js/not/Board.java
+++ src/com/better/than/yours/game/cucumbers/js/not/Board.java
@@ -1,7 +1,7 @@
 package com.better.than.yours.game.cucumbers.js.not;
 
 class Board {
-    static int width ; // width and height of the board
+    static int width ; 
     static int height;
     Board(int width,int height){
         Board.width =width;
